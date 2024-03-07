0572d35017f5527e2aaac7fdd26bd7f7808272aa
giovannadexter <141785018+brasilgabri@users.noreply.github.com>
2024-03-07 20:21:55 -0300

    KJwajkdasldjlkaskl

--- ONGWeb2/src/js/index.js
+++ ONGWeb2/src/js/index.js
@@ -1,3 +1,37 @@
+
+
+window.addEventListener('scroll', function() {
+
+  var div = document.getElementById('ong-card');
+
+  if (isElementInViewport(div)) {
+
+      div.addEventListener('click', function() {
+        
+        var url = div.getAttribute('data-url');
+        if (url) {
+            window.location.href = url;
+        }  
+      
+      });
+
+      //div.style.display = 'block';
+
+      // Remove the scroll listener to prevent multiple listeners
+      window.removeEventListener('scroll', arguments.callee);
+  }
+});
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
 function listOngs() {
     
     var container = document.getElementById('ongs-container');
